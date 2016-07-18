026e45eab9307d4a97ce299e6e1a2c9ae3b61e7d
theMohammedA <mohammedabdulgaffar@outlook.com>
2016-07-18 21:21:53 +0400

    cleaning & dashboard status

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Products;//Products model.
+use App\Customers;//Customers model to fetch customers.
+use App\Inventory;//Inventory model.
+use App\Quotations;//Quotations model.
+use App\QuoteItems;//QuoteItems model.
 
 class AdminController extends Controller
 {
@@ -16,6 +21,17 @@ class AdminController extends Controller
     // Show the application dashboard.
     public function index()
     {
-        return view('dashboard',['title' => 'Dashboard']);
+        
+        $products = Products::count();
+        $inventory = Inventory::sum('vendor_quantity');
+        $customers = Customers::count();
+        $quotations = Quotations::count();
+
+        return view('dashboard',['title' => 'Dashboard',
+        'products' => $products,
+        'inventory' => $inventory,
+        'customers' => $customers,
+        'quotations' => $quotations
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,93 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="content-container">
+<div id="dashboard" class="content-container">
 
-</div>
+ <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>{{$quotations}}</h3>
+
+              <p>Quotations</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="{{URL::to('\quotations')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>{{$products}}</h3>
+
+              <p>Products</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="{{URL::to('\products')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>{{$customers}}</h3>
+
+              <p>Customers</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="{{URL::to('\customers')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>{{$inventory}}</h3>
+
+              <p>Total Items</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="{{URL::to('\products')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+
+</div><!-- end of container -->
+<!-- Datatables -->
+
+<script src="{{URL::to('/')}}/adminlte/plugins/datatables/dataTables.min.js"></script>
+<link rel="stylesheet" type="text/css" href="{{URL::to('/')}}/adminlte/plugins/datatables/datatables.min.css"></link>
+
+<script type="text/javascript">
+$('.btn-addnew').on( 'click', function () {
+	window.location.href = "{{URL::to('/')}}/customers/create";
+});
+jQuery(function () {
+    jQuery('#table_quotations').DataTable({
+    "paging": true,
+    "lengthChange": true,
+    "searching": true,
+    "ordering": true,
+    "info": false,
+    "responsive": true,
+    "autoWidth": true
+    });
+});
+</script>
 @endsection
